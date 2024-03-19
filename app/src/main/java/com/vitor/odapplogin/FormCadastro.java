1da2a2defb0034e9d92d762a0b74488810621599
Vitor Utimura Locateli <vitorul04@gmail.com>
2024-03-19 15:58:30 -0300

    Teste ODAPP

---
+++ app/src/main/java/com/vitor/odapplogin/FormCadastro.java
@@ -0,0 +1,16 @@
+package com.vitor.odapplogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+    }
+}
